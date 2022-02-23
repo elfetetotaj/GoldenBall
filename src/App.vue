89d9dc99bf1e39d8b38449712a8ed476dffd4526
Elfete Totaj <et48436@ubt-uni.net>
2022-02-23 20:46:43 +0100

    Slider

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <Header/>
     <router-view />
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Footer />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
+        <Slider />
         <h2>Hello from home</h2>
+        <Contact-form />
     </div>
 </template>
 <script>
+import Slider from '../Components/HomePage/Slider.vue'
+import ContactForm from '../Components/ContactPage/ContactForm.vue'
 export default {
+  components: { Slider, ContactForm },
     name:'Home',
 }
 </script>
